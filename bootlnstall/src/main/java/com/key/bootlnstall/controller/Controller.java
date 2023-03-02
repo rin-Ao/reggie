b767914d8d1800d35e7ebc236169ba06ad171925
rin <hello@rin.cn>
2023-03-02 17:12:47 +0800

    。

--- bootlnstall/src/main/java/com/key/bootlnstall/controller/Controller.java
+++ bootlnstall/src/main/java/com/key/bootlnstall/controller/Controller.java
@@ -1,7 +1,16 @@
 package com.key.bootlnstall.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
 public class Controller {
 
-
-
+    @GetMapping()
+    public String hello(){
+        System.out.println("hi");
+        return "ok";
+    }
 }
