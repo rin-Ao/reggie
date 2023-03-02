12eb10b84ec5a01d1252598023f921fbb20584d9
rin <hello@rin.cn>
2023-03-02 17:20:02 +0800

    。

---
+++ bootlnstall/src/test/java/com/key/bootlnstall/BootlnstallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.key.bootlnstall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootlnstallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
